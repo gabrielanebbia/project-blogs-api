2382b82ba929aee77432288925c9c91aea0cf2a3
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-03-29 20:47:45 -0300

    chore: seed para Users

---
+++ seeders/20220329233353-users.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert('Users',
+      [{
+        id: 1,
+        displayName: 'Margaret Hamilton',
+        email: 'margarethamilton@gmail.com',
+        password: 'MIT@polo1!1936',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Margaret_Hamilton_1995.jpg/475px-Margaret_Hamilton_1995.jpg',
+      },
+      {
+        id: 2,
+        displayName: 'Dorothy Vaughan',
+        email: 'dorothyvaughan@gmail.com',
+        password: 'N@S@boss1910',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Dorothy_Vaughan.jpg/291px-Dorothy_Vaughan.jpg',
+      },
+      ], { timestamps: false });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
