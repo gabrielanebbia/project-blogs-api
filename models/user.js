896286707d5ae89c0bdd46a011b12a99f2903aa7
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-03-29 20:23:48 -0300

    refactor: altera model de Users de classes para função

--- models/user.js
+++ models/user.js
@@ -1,26 +1,20 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  const User = sequelize.define(
+    'User',
+    {
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false, tableName: 'Users' },
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'blogPost',
+    });
+  };
+
   return User;
 };
