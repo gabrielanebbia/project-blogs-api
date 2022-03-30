2dbaf1013a3ccf9ceae6fc047fe9b501dcfdf98d
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-03-30 00:47:19 -0300

    test(user): adiciona checkModelName

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,6 +1,7 @@
 const {
   sequelize,
   dataTypes,
+  checkModelName,
 } = require('sequelize-test-helpers');
 
 const UserModel = require('../../../models/user');
@@ -8,5 +9,9 @@ const UserModel = require('../../../models/user');
 describe('src/models/user', () => {
   const User = UserModel(sequelize, dataTypes);
   const user = new User();
+
+  describe('Model name is "User"', () => {
+    checkModelName(User)('User');
+  });
 
 });
